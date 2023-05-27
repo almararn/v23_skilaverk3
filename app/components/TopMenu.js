c63cb92186363a3ea936c1740dc71a1024ed4178
almararn <almararn@gmail.com>
2023-05-27 17:44:25 +0000

    top menu

---
+++ app/components/TopMenu.js
@@ -0,0 +1,13 @@
+export default function TopMenu() {
+  return (
+    <nav>
+      <ul className="bg-red-400 flex justify-evenly py-4 mb-2 font-semibold text-lg">
+        <li>LOGO</li>
+        <li>Restaurants</li>
+        <li>Menus</li>
+        <li>Opening Hours</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
