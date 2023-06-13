81648f4e39dc914d16c348d9ec3312f8d7125faf
almararn <almararn@gmail.com>
2023-06-13 13:53:22 +0000

    vercel fix

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,7 +2,7 @@ const footer = () => {
   return (
     <footer className="fixed bottom-0 w-screen z-50">
         <div className="bg-lil-red flex h-16 justify-evenly py-0 font-semibold text-lg">
-            <div className="mt-6 text-black opacity-75 text-base">&copy; Lil'Bits 2023</div>
+            <div className="mt-6 text-black opacity-75 text-base">&copy; Lil&apos;Bits 2023</div>
         </div>
     </footer>
   )
